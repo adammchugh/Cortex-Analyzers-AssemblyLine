1732371ee3cbae75d445cd89a5084f3963167c5f
Adam McHugh <adammchugh83@gmail.com>
2021-02-21 16:00:26 +1030

    Inclusion of Auth Parameters

--- assemblyline/assemblyline.py
+++ assemblyline/assemblyline.py
@@ -2,9 +2,9 @@
 # encoding: utf-8
 
 import time
-import assemblyline
 
 from cortexutils.analyzer import Analyzer
+from assemblyline_client import get_client
 
 class Assemblyline(Analyzer):
 
@@ -13,13 +13,20 @@ class Assemblyline(Analyzer):
     self.service = self.get_param('config.service', None, 'Service parameter is missing')
     self.polling_interval = self.get_param('config.polling_interval', 60)
     self.proxies = self.get_param('config.proxy', None)
+    self.assemblyline_server = self.get_param('config.assemblyline_server',None)
+    self.assemblyline_user = self.get_param('config.assemblyline_user',None)
+    self.assemblyline_key = self.get_param('config.assemblyline_key',None)
+    self.assemblyline_verifyssl = self.get_param('config.assemblyline_verifyssl',True)
 
   def artifacts(self, raw):
     return [self.build_artifact(self.data_type, self.getData(), tags=["assemblyline"], tlp=self.tlp)]
 
   def run(self):
-    if self.data_type not in ['domain', 'ip']:
+    if self.data_type not in ['file']:
       self.notSupported()
+
+    al_client = get_client(self.assemblyline_server, apikey=(self.assemblyline_user, self.assemblyline_key), verify=self.assemblyline_verifyssl)
+    al_client.submit('/path/to/my/file.txt')
 
     time.sleep(self.getParam("config.delay", 60))
 
